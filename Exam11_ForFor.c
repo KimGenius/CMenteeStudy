5e2388c6271668fdd18a1c6150b06d1b2e47246b
Kim0Jae <youngjae1047@naver.com>
2017-07-13 02:38:33 +0900

    Add more exam

--- Exam11_ForFor.c
+++ Exam11_ForFor.c
@@ -5,10 +5,17 @@
 #include <stdio.h>
 
 int main() {
-    for (int i = 0; i < 5; ++i) {
-        for (int j = 0; j < 5; ++j) {
-            printf("i : %d, j : %d\n", i, j);
+//    for (int i = 0; i < 5; ++i) {
+//        for (int j = 0; j < 5; ++j) {
+//            printf("i : %d, j : %d\n", i, j);
+//        }
+//    }
+    int i, j;
+    for (i = 1; i <= 9; i++) { // 2 3
+        for (j = 1; j <= 9; j++) { //1 2
+            printf("%d X %d = %d\n", i, j, i * j); //2 X 9 = 4
         }
+        printf("\n");
     }
     return 0;
 }
